3e49afb27cf00d933f6fa4ad79f5d6373a365d0e
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-26 17:34:41 +0300

    Code Style

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,9 @@ const nextConfig = {
 
     return config;
   },
+  experimental: {
+    typedRoutes: true,
+  }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      </body>
     </html>
   );
 }
